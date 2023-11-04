775950d715d9ea7cd315ae2da73737d542610867
Fereshteh136 <149617279+Fereshteh136@users.noreply.github.com>
2023-11-04 13:33:46 +0330

    Add files via upload

---
+++ Double.py
@@ -0,0 +1,10 @@
+#Input a number as a string
+user_input = input("Enter a number: ")
+
+# Convert the input to an integer
+number = int(user_input)
+
+# Perform a mathematical operation
+result = number * 2
+
+print(f"Double of {number} is {result}.")
